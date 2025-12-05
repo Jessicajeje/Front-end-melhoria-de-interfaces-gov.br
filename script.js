928287892444fdaced96f24cb034568a51353f73
Jéssica Steffany <jessicasteffany598@gmail.com>
2025-12-05 10:01:14 -0300

    Remove 'Concluir' button click event listener
    Removed event listener for the 'Concluir' button.

--- script.js
+++ script.js
@@ -75,16 +75,5 @@ localStorage.setItem("isEstudante", isEstudante);
     });
 });
 
-//para o botão "Concluir"
-botaoConcluir.addEventListener('click', function() {
-    if (isEstudante) {
-        
-        alert('Cadastro confirmado, redirecionando para menu Estudantil govbr.');
-        window.location.href = 'inicioEstudante.html';
-    } else {
-        alert('Cadastro confirmado, redirecionando para menu Cidadão govbr.');
-       window.location.href="inicioCidadao.html"
-    }
-});
 
 
