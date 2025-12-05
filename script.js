722bc4fc763c4c455cb0ddada0428e9dd8726630
Jéssica Steffany <jessicasteffany598@gmail.com>
2025-12-05 10:00:46 -0300

    Remove 'Concluir' button event listener
    Removed event listener for the 'Concluir' button that handled redirection based on user type.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function irMensagem(){
   window.location.href="mensagemPostal.html";
 }
 function irSenha(){
-  window.location.href="loginSenha.html"
+  window.location.href="loginSenha.html";
 }
 function irCadastro(){
   window.location.href="cadastro.html";
@@ -74,17 +74,4 @@ botoesSelect.forEach(botao => {
 localStorage.setItem("isEstudante", isEstudante);
     });
 });
-
-//para o botão "Concluir"
-botaoConcluir.addEventListener('click', function() {
-    if (isEstudante) {
-        
-        alert('Cadastro confirmado, redirecionando para menu Estudantil govbr.');
-        window.location.href = 'inicioEstudante.html';
-    } else {
-        alert('Cadastro confirmado, redirecionando para menu Cidadão govbr.');
-       window.location.href="inicioCidadao.html"
-    }
-});
-
 
